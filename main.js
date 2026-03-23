bf40863a1f8052a3ea216cbe86998e779ef7be6d
vividani <110170717+vividani@users.noreply.github.com>
2026-03-23 11:10:52 +0900

    2nd Modify

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 $(function(){
     new fullpage('#fullpage', {
+        autoScrolling: true,
         navigation: true,
         navigationPosition: 'right',
         responsiveWidth: 700,
         anchors: ['home', 'bestsellers', 'gifts', 'mens', 'supports', 'footer'],
-        parallax: true,
+        // parallax: true,
         onLeave: function(origin, destination, direction){
             console.log("Leaving section" + origin.index);
         },
         verticalCentered: false,
         paddingBottom: '0px',
+        licenseKey: 'gplv3-license',
     });
+
+
+    document.querySelector("#p5__txt__container")
 })
